ae0e123934cb040369dae8402c8f5bdf04321fba
Mariusz Ryndzionek <mariusz.ryndzionek@mpicosys.com>
2016-03-08 11:23:48 +0100

    A minor formatting adjustment

--- models/generate.sh
+++ models/generate.sh
@@ -2,7 +2,7 @@
 
 command -v gsl >/dev/null 2>&1
 if  [ $? -ne 0 ]; then
-    echo "$0: error: could not find gsl.  gsl is required to run $0." 1>&2
+    echo "$0: error: could not find gsl. gsl is required to run $0." 1>&2
     exit 1
 fi
 
